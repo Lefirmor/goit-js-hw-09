84809441adc2835d6946f5b40f6ed18e0a3d0a4f
Lefirmor <111429897+Lefirmor@users.noreply.github.com>
2023-02-23 20:55:18 +0000

    Deploying to gh-pages from @ Lefirmor/goit-js-hw-09@737a51e3047a106b689524e4b176f6d9cf16b8b2 🚀

---
+++ 01-color-switcher.8d4b2d5a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(()=>{setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.disabled=!0,t.disabled=!1}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.8d4b2d5a.js.map
